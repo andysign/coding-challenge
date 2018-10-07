288709e52fd344bbdbc6ea686f5a62e27fc8413e
andysign <a.baloiu@greenrunning.com>
2018-10-07 04:27:33 +0100

    Added comments

--- challenge3/interleaved-bin.py
+++ challenge3/interleaved-bin.py
@@ -5,11 +5,13 @@
 """
 
 # Import sys and q and itertools
-import sys, asyncio, itertools
-
-queue = asyncio.Queue()
+import sys
+import asyncio
+import itertools
 
 strarg = sys.argv[1] if len(sys.argv)-1 else "XX"
+
+queue = asyncio.Queue()
 
 def interprint(strvar):
 	strfixed = list(strarg.split("X"))
@@ -17,20 +19,28 @@ def interprint(strvar):
 	interleaved = itertools.zip_longest(strfixed,strvar,fillvalue='')
 	return "".join([x for t in interleaved for x in t])
 
+# Calculate exponential power to calculate n
 exp = len(strarg.replace("1","").replace("0",""))
 n = 2**exp - 1
 
+# Enqueu the first and second number
 queue.put_nowait("0")
 queue.put_nowait("1")
 
+# Print first
 print(interprint( queue.get_nowait().rjust(exp, "0") ))
 
+# Make a loop that makes a tree with 1 root 0 as l child and 1 as r child etc
 for i in range(n,0,-1):
+	# Print the front of the queue
 	f1 = queue.get_nowait()
 	print(interprint( f1.rjust(exp,"0") ))
 	
+	# Backup f1
 	f2 = f1
 	
+	# Append "0" to f1 and enqueue it
 	queue.put_nowait(f1 + "0")
 	
+	# Append "1" to f2 and enqueue it. Here f2 contains the prev front
 	queue.put_nowait(f2 + "1")
