3d3f49a2fab0056590cb360efa7328b3e6fc518d
andysign <a.baloiu@greenrunning.com>
2018-10-08 04:02:37 +0100

    Moved comment at the end

--- challenge2/fintems.js
+++ challenge2/fintems.js
@@ -2,11 +2,11 @@
 /*
 * Script crafted to to find one pair of prices from a sorterd list of prices
 * that when combined the resulted value is equal or less then a given total.
-* Usage: ./fintem.js ["FILE"] ["TOTAL"]
+* Usage: ./fintems.js ["FILE"] ["TOTAL"]
 * Where: "FILE" is the file name/path built like: Headphones, 1400\nBox, 100\n
 * And where: "TOTAL" is the desired total of the pair
 * Will return: the pair of items with prices or "Not possible"
-* Usage2: node fintem.js ["FILENAME"] ["TOTAL"]
+* Usage2: node fintems.js ["FILENAME"] ["TOTAL"]
 */
 
 // Require the file system npm to help read the file
@@ -34,14 +34,9 @@ items = filestr.split('\n');
 prices = items.map( function(e){ return 1*e.split(', ')[1]; } );
 names  = items.map( function(e){ return e.split(', ')[0]+' '; } );
 
-// Old version with hash map, not working for best x and y only for fix match
-// mp = new Map(); T=5;  for (i=0; i<prices.length; i++) mp.set( prices[i] , T - prices[i] );
-// for (i=0;i<prices.length;i++) if (mp.get( T/*12*/ - i )) console.log(T-i, mp.get(T-i));
-
 // Init the best modified binary search vars
-let left = 0, right = prices.length-1, found = false;
-let bestx = besty = bestnx = bestny = null;
-// console.log(prices); console.log("T",t);
+let left = 0, right = prices.length-1, found = false; // console.log(prices);
+let bestx = besty = bestnx = bestny = null; // console.log("T",t);
 
 while(left<right) { //console.log("lr", left, right)
 	x = prices[left]; y = prices[right]; //console.log("newxy",bestx,besty);
@@ -66,8 +61,13 @@ while(left<right) { //console.log("lr", left, right)
 	}
 	if(found) break; //console.log("---")
 }
+
 if ( bestx!=null && besty!=null ) {
 	console.log(bestnx+bestx +', '+ bestny+besty);
 } else {
 	console.log("Not possible");
 }
+
+// Old version with hash map, not working for best x and y only for fix match
+// mp = new Map(); T=5;  for (i=0; i<prices.length; i++) mp.set( prices[i] , T - prices[i] );
+// for (i=0;i<prices.length;i++) if (mp.get( T/*12*/ - i )) console.log(T-i, mp.get(T-i));
