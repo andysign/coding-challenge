9b56ce7cf4c6b2d8d7fdd4bd8897694454210801
andysign <a.baloiu@greenrunning.com>
2018-10-08 03:21:14 +0100

    Fixed bug in last best xy , check for fullpath and fixed comments

--- challenge2/fintem.js
+++ challenge2/fintem.js
@@ -2,8 +2,8 @@
 /*
 * Script crafted to to find one pair of prices from a sorterd list of prices
 * that when combined the resulted value is equal or less then a given total.
-* Usage: ./fintem.js ["FILENAME"] ["TOTAL"]
-* Where: "FILENAME" is the file similar to: Headphones, 1400\nBox, 100\n
+* Usage: ./fintem.js ["FILE"] ["TOTAL"]
+* Where: "FILE" is the file name/path built like: Headphones, 1400\nBox, 100\n
 * And where: "TOTAL" is the desired total of the pair
 * Will return: the pair of items with prices or "Not possible"
 * Usage2: node fintem.js ["FILENAME"] ["TOTAL"]
@@ -22,27 +22,34 @@ const total = process.argv[3];
 const fn = filename ? filename : 'prices.txt';
 const t  = total ? total : 2500;
 
+// Read the file and store the content in a simple str
 __dirname = path.resolve(path.dirname('')) + '/';
-fpath = __dirname + '/' + fn; filestr = fs.readFileSync(fpath, {encoding:'utf8'}).trim(); items = filestr.split('\n');
+__dirname = (fn.includes('/')||fn.includes('\\')) ? '' : __dirname;
+const fpath = __dirname + '/' + fn;
+const filestr = fs.readFileSync(fpath, {encoding:'utf8'}).trim();
 
-
+// Get the prices and names
+let items = [], prices = [], names = [];
+items = filestr.split('\n');
 prices = items.map( function(e){ return 1*e.split(', ')[1]; } );
-
 names  = items.map( function(e){ return e.split(', ')[0]+' '; } );
 
 // Old version with hash map, not working for best x and y only for fix match
 // mp = new Map(); T=5;  for (i=0; i<prices.length; i++) mp.set( prices[i] , T - prices[i] );
 // for (i=0;i<prices.length;i++) if (mp.get( T/*12*/ - i )) console.log(T-i, mp.get(T-i));
 
-left = 0; right = prices.length-1; bestx = null; besty = null; found = false;
+left = 0; right = prices.length-1; found = false;
+bestx = null; besty = null; bestnx = null; bestny = null;
+
+console.log(prices); console.log("T",t);
 
 while(left<right) {
-x = prices[left]; y = prices[right]; console.log(x,y);
-if ( x+y == t ) { console.log("F"); found = true; bestx = x; besty = y; }
-if ( x+y > t ) { console.log("B"); right--; }
-if ( x+y < t ) { console.log("s"); left++; bestx = x; besty = y; }
-console.log("lr,t", left, right, t)
-console.log(prices)
+console.log("lr", left, right)
+x = prices[left]; y = prices[right]; console.log("newxy",bestx,besty);
+if ( x+y == t ) { console.log("FOUND"); found = true; bestnx = names[left]; bestny = names[right]; bestx = x; besty = y; }
+if ( x+y > t ) { console.log("XYTOBIG"); right--; }
+if ( x+y < t ) { console.log("small"); bestnx = names[left]; bestny = names[right]; bestx = x; besty = y; left++; }
 if(found) break;
+console.log("---")
 }
-if (bestx!=null&&besty!=null) {console.log(names[left]+prices[left] +', '+ names[right]+prices[right]);} else {console.log("Not possible");}
+if (bestx!=null&&besty!=null) {console.log(bestnx+bestx +', '+ bestny+besty);} else {console.log("Not possible");}
