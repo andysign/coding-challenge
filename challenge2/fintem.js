1fc5a5bef2bf621850ff0065e1ef6d28f809591b
andysign <a.baloiu@greenrunning.com>
2018-10-08 02:44:37 +0100

    Separated req and more comments

--- challenge2/fintem.js
+++ challenge2/fintem.js
@@ -3,19 +3,27 @@
 * Script crafted to to find one pair of prices from a sorterd list of prices
 * that when combined the resulted value is equal or less then a given total.
 * Usage: ./fintem.js ["FILENAME"] ["TOTAL"]
-* Where: ["FILENAME"] is the file similar to: Headphones, 1400\nBox, 100\n
-* And where: ["TOTAL"] is the desired total of the pair
+* Where: "FILENAME" is the file similar to: Headphones, 1400\nBox, 100\n
+* And where: "TOTAL" is the desired total of the pair
 * Will return: the pair of items with prices or "Not possible"
 * Usage2: node fintem.js ["FILENAME"] ["TOTAL"]
 */
 
+// Require the file system npm to help read the file
 const fs = require('fs');
+// Require path to help build the full path
+const path = require('path');
 
+// Get the command prompt filename and price arguments
 const filename = process.argv[2];
 const total = process.argv[3];
-fn = filename ? filename : 'prices.txt';
-t  = total ? total : 2500;
-path = require('path'); __dirname = path.resolve(path.dirname('')); fpath = __dirname + '/' + 'prices.txt'; filestr = fs.readFileSync(fpath, {encoding:'utf8'}).trim(); items = filestr.split('\n');
+
+// Get the filename and price or default to predefined
+const fn = filename ? filename : 'prices.txt';
+const t  = total ? total : 2500;
+
+__dirname = path.resolve(path.dirname('')) + '/';
+fpath = __dirname + '/' + fn; filestr = fs.readFileSync(fpath, {encoding:'utf8'}).trim(); items = filestr.split('\n');
 
 
 prices = items.map( function(e){ return 1*e.split(', ')[1]; } );
