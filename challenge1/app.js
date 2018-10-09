bf5fe9f4bf9ddf45c276965cb77604446714a6b9
andysign <a.baloiu@greenrunning.com>
2018-10-09 20:16:58 +0100

    Simplified the code. optimized check before save and made the the functions ES2016+ friendly

--- challenge1/app.js
+++ challenge1/app.js
@@ -3,7 +3,7 @@
 * the original messages based on digest hashes
 * USAGE:
 * RUNNING: node app.js
-* TEST: open browser and naviget to
+* TEST: open browser and naviget to <IP>/ or
 * http://<IP>:8080/messages
 * http://<IP>:8080/messages/12...
 */
@@ -25,30 +25,30 @@ const MLABACC= 'andy-bit';
 let db;
 
 // Index page as in domain.com/
-app.get('/', function(req,res) {
+app.get('/', (req,res)=>{
 	res.send('<html><head><title>Index</title></head><body>'+
 	'<b>Move along, nothing to see here!</b> <a href="/messages">form</a>?'+
 	'</body></body></html>');
-}).post('/', function(req,res){ res.status(404).end('404'); });
+}).post('/', (req,res)=>{ res.status(404).end('404'); });
 // Create router for /messages and /messages/HASH
 app.use(parser.urlencoded({extended:true}))
 	.use('/messages', router);
 // Create form and db save function
-router.get('', function(req,res) {
+router.get('', (req,res)=>{
 	res.send('<html><head><title>Sha</title></head><body>'+
-	'<h2>Submit message for sha256 hashing</h2>'+
+	'<h2>Submit message for sha256 hashing and saving</h2>'+
 	'<form action="/messages" method="POST">'+
 	'<input placeholder="foo" name="message">'+
 	'<button type="submit">Submit</button>'+
 	'</form></body></body></html>');
-}).post('', function(req,res) {
+}).post('', (req,res)=>{
 	let message = req.body.message;
 	let hash = crypto.createHash('sha256').update(message).digest('hex');
-	let th = this;//,{projection:{_id:0,hash:0}}
-	let dbcol = db.collection('messages');
-	dbcol.find({hash:hash}).limit(1).toArray(function(e,r){
+	let ms = db.collection('messages');
+	// Save if not already in there. Can be optimized!
+	ms.find({hash:hash},{projection:{_id:0,hash:0}}).limit(1).toArray((e,r)=>{
 		if (r.length==0) {
-			dbcol.save({message:message,hash:hash},(err,rep)=>{
+			ms.save({message:message,hash:hash},(err,rep)=>{
 				if (err) { console.log(0); return false; }
 				res.send(/*'Not saved before ' +*/ hash);
 			});
@@ -58,12 +58,13 @@ router.get('', function(req,res) {
 	});
 });
 // Get the GET hash and spit out the original msg or else show 404 error
-router.get('/:hash([a-fA-F0-9]+)', function(req,res) {
+router.get('/:hash([a-fA-F0-9]+)', (req,res)=>{
 	res.set({'Content-Type': 'application/json'});
 	let hash = req.params.hash;
+	// Find and get msg. 
 	db.collection('messages')
 	.find({hash:hash},{projection:{_id:0,hash:0}}).limit(1)
-	.toArray(function(e,r){
+	.toArray((e,r)=>{
 		if (!e) {
 			if (r.length) {
 				res.send(JSON.stringify(r[0],null,' '));
@@ -73,7 +74,7 @@ router.get('/:hash([a-fA-F0-9]+)', function(req,res) {
 			}
 		} else {res.send('["connection_error"]')}
 	});
-}).post('/:hash', function(req,res) {
+}).post('/:hash', (req,res)=>{
 	res.send("No POST allowed, just GET");
 });
 
